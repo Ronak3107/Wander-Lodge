88b4c72a3bd2142a31cd1b420c035f1a58c275a8
Ronak Sharma <rvronaksharma3107@gmail.com>
2024-03-28 21:08:39 +0530

    first commit

---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,13 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+        Welcome Home! Anywhere you roam <br /> Embrace each moment, and let memories grow.
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
